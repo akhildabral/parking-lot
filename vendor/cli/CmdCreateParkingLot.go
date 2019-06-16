e0381388523132adb4eea65a52526283beeb4f23
Akhil Dabral <akhil.dabral.contractor@move.com>
2019-06-17 00:37:21 +0530

    Added Some more comments

--- vendor/cli/CmdCreateParkingLot.go
+++ vendor/cli/CmdCreateParkingLot.go
@@ -6,47 +6,56 @@ import (
 	"strconv"
 )
 
+//cmdCreateParkingLot - CreateParkingLot command object, implementing ICommand interface
 type cmdCreateParkingLot struct {
-	ICommand
-	noOfArgs int
-	name     string
+	ICommand        // ICommand Interface
+	noOfArgs int    //Number of arguments required for the command
+	name     string // Name of the command
 }
 
+//CmdCreateParkingLot - Create Instance of cmdCreateParkingLot Struct
 func CmdCreateParkingLot() ICommand {
 	c := &cmdCreateParkingLot{}
 	c.Init()
 	return c
 }
 
+//Init - Initialize command with No Of Arguments and Command Name
 func (cmd *cmdCreateParkingLot) Init() {
 	cmd.noOfArgs = 1
 	cmd.name = "create_parking_lot"
 }
 
+//Run - Executes Command
+// args : Arguments for the command
 func (cmd *cmdCreateParkingLot) Run(args []string) {
+	//Check is arguments are valid
 	valid := cmd.Validate(args)
 
+	//If not, the show help to the user and retur
 	if !valid {
 		cmd.Help()
 		return
 	}
 
+	//Do What Command is intented to Do...
 	cap, _ := strconv.Atoi(args[0])
 	parking.CreateParking(cap)
 
 }
 
+//Validate - Validated the Arguments for the command
+//args : list of arguments
 func (cmd *cmdCreateParkingLot) Validate(args []string) bool {
+	//Check if arguments length matched the required length
 	if len(args) == cmd.noOfArgs {
 		return true
 	}
 	return false
 }
 
-func (cmd *cmdCreateParkingLot) ProcessInput() {
-
-}
-
+//Help - Show help to the User
 func (cmd *cmdCreateParkingLot) Help() {
-	fmt.Println("You Need SOme help Creating Paking!")
+	fmt.Println(`Help --> 
+create_parking_lot :	<number of slots>  => (Create a parking lot of specified size)`)
 }
--- vendor/cli/cli.go
+++ vendor/cli/cli.go
@@ -7,40 +7,53 @@ import (
 	"strings"
 )
 
+//Cli - Command Line Interface
 type Cli struct {
-	cmdHandler *commandHandler
+	cmdHandler *commandHandler //Hold pointer to CommandHandlere
 }
 
+//CreateCli - Instanciate CLI
 func CreateCli() *Cli {
 	c := &Cli{}
 	c.Init()
 	return c
 }
 
+//Init - Initialize
 func (c *Cli) Init() {
+	//Create Command Handler object and assign to Cli object
 	c.cmdHandler = CreateCmdHandler()
 }
 
+//ShowPrompt - Display Prompt
 func (c *Cli) ShowPrompt() {
 	fmt.Print("$ ")
 }
 
+//Execute - Execute CLI Process
 func (c *Cli) Execute() {
+	//Read Input Streame
 	reader := bufio.NewReader(os.Stdin)
 	c.ShowPrompt()
+	//Keep reading input from Stdin untill 'exit'
 	for {
+		//Read input till new line(enter) is presses
 		cmdInput, _ := reader.ReadString('\n')
+		//Trim new Line character from the end
 		cmdInput = strings.TrimRight(cmdInput, "\n")
 
+		//Check if exit is types, Break the loop and close process
 		if cmdInput == "exit" {
 			return
 		}
 
+		//Is Cmd is not empty, Process Command, Else show help
 		if "" != cmdInput {
+			//Pass command to the handler
 			out, err := c.cmdHandler.Run(cmdInput)
 			c.cmdHandler.processOutput(out, err)
 		} else {
-			fmt.Println("HELPPPPP")
+			//Show Help Message
 			c.cmdHandler.commands["help"].Run(nil)
 		}
 		c.ShowPrompt()
